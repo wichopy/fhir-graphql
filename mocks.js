3ffaa0407f8fb2f355b81896981c606c50b5de0e
William Chou <w.chou06@gmail.com>
2018-06-30 13:45:42 -0400

    Fill in non fhir specific fields with Casual fake data generator.

--- mocks.js
+++ mocks.js
@@ -1,6 +1,25 @@
+import casual from 'casual'
+
 const mocks = {
-  String: () => 'It works!',
   Date:   () => new Date().toISOString(),
+  Patient: () => ({
+    resourceType: "Patient",
+    id: () => String(casual.integer(from = 0, to = 1000)),
+  }),
+  HumanName: () => ({
+    given: () => [casual.first_name, casual.first_name],
+    family: () => casual.last_name,
+    prefix: () => [casual.name_prefix, casual.name_prefix],
+    suffix: () => [casual.name_suffix, casual.name_suffix,],
+    text: () => casual.full_name,
+  }),
+  String: () => 'It works!',
+  Address: () => ({
+    country: casual.country_code,
+    postalCode: casual.zip,
+    city: casual.city,
+    text: casual.address,
+  })
 };
 
 export default mocks;
