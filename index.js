ebb9e7de99d03f775bbcc980cd10d7c313d67d50
William from DO <w.chou06@gmail.com>
2018-07-01 14:07:09 +0000

    Handle basenames

--- index.js
+++ index.js
@@ -3,13 +3,15 @@ import bodyParser from 'body-parser';
 import { graphqlExpress, graphiqlExpress } from 'apollo-server-express';
 import schema from './schema'
 
-const PORT = 3000;
+const PORT = 3005;
 
 const app = express();
 
+const baseName = '/fhir-graphql';
+
 // bodyParser is needed just for POST.
-app.use('/graphql', bodyParser.json(), graphqlExpress({ schema }));
-app.get('/graphiql', graphiqlExpress({ endpointURL: '/graphql' })); // if you want GraphiQL enabled
+app.use(baseName + '/graphql', bodyParser.json(), graphqlExpress({ schema }));
+app.get(baseName + '/graphiql', graphiqlExpress({ endpointURL: baseName + '/graphql' })); // if you want GraphiQL enabled
 
 console.log('listening on port ', PORT)
 app.listen(PORT);
