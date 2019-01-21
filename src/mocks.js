5cfa815bece52682b99c7e64fc5276695d425fcb
ronniemara <ronald.marangwanda@gmail.com>
2019-01-21 13:21:27 -0500

    Feature patient query (#14)
    * added patient to resolvers
    
    * added args
    
    * :blush: comma
    
    * fixed linting issues
    
    * airbnb style guide
    
    * extends
    
    * fixed some thangs
    
    * Fix lint errors, add missing dep, fix start script.

--- src/mocks.js
+++ src/mocks.js
@@ -1,44 +1,44 @@
-import casual from "casual"
+import casual from 'casual';
 
-casual.define("fhir_medication_statement", function() {
-  return "MedicationStatement/123"
-})
+casual.define('fhir_medication_statement', () => {
+  return 'MedicationStatement/123';
+});
 
 const mocks = {
-  Date:   () => new Date().toISOString(),
+  Date: () => new Date().toISOString(),
   Patient: () => ({
-    resourceType: "Patient",
-    id: () => String(casual.integer(0,1000)),
+    resourceType: 'Patient',
+    id: () => String(casual.integer(0, 1000))
   }),
   Meta: () => ({
-    versionId: () => String(casual.integer(0,1000)),
+    versionId: () => String(casual.integer(0, 1000))
   }),
   HumanName: () => ({
     given: () => [casual.first_name, casual.first_name],
     family: () => casual.last_name,
     prefix: () => [casual.name_prefix, casual.name_prefix],
-    suffix: () => [casual.name_suffix, casual.name_suffix,],
-    text: () => casual.full_name,
+    suffix: () => [casual.name_suffix, casual.name_suffix],
+    text: () => casual.full_name
   }),
-  String: () => "It works!",
+  String: () => 'It works!',
   Address: () => ({
     country: casual.country_code,
     postalCode: casual.zip,
     city: casual.city,
-    text: casual.address,
+    text: casual.address
   }),
   QuestionnaireResponseItem: () => ({
-    definition: casual.text,
+    definition: casual.text
   }),
   QuestionnaireResponseAnswer: () => ({
     valueDate: new Date().toISOString(),
     valueBoolean: casual.boolean,
-    valueFloat: casual.double(0,100),
-    valueInteger: casual.integer(0, 1000),
+    valueFloat: casual.double(0, 100),
+    valueInteger: casual.integer(0, 1000)
   }),
   Reference: () => ({
-    reference: casual.fhir_medication_statement,
+    reference: casual.fhir_medication_statement
   })
-}
+};
 
-export default mocks
+export default mocks;
--- src/resolverHelpers.js
+++ src/resolverHelpers.js
@@ -2,16 +2,18 @@ export const findByLinkId = (questionnaireResponse, linkId) => {
   // recursive function to traverse a questionnaire response with multi-nested items.
   const traverseQR = (QRItem) => {
     if (QRItem.linkId === linkId) {
-      return QRItem
+      return QRItem;
     }
 
     if (questionnaireResponse.item) {
+      // eslint-disable-next-line
       for (let item of QRItem.item) {
-        return traverseQR(item)
+        return traverseQR(item);
       }
     }
 
-  }
+    return null;
+  };
 
-  return traverseQR(questionnaireResponse)
-}
+  return traverseQR(questionnaireResponse);
+};
